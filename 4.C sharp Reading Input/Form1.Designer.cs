788c9bf43bcc74bd603e130143bc9c6b51cc9e1f
Mark Lusala Mudoga <lusalamark63@gmail.com>
2021-03-22 16:29:00 +0300

    First Commit

---
+++ 4.C sharp Reading Input/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace _4.C_sharp_Reading_Input
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.txtInput = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// txtInput
+			// 
+			this.txtInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.txtInput.Location = new System.Drawing.Point(12, 24);
+			this.txtInput.Name = "txtInput";
+			this.txtInput.Size = new System.Drawing.Size(1171, 62);
+			this.txtInput.TabIndex = 3;
+			this.txtInput.TextChanged += new System.EventHandler(this.txtInput_TextChanged_2);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1247, 519);
+			this.Controls.Add(this.txtInput);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+		private System.Windows.Forms.TextBox txtInput;
+	}
+}
+
